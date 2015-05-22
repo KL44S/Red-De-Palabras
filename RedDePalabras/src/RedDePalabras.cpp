e91d2600c849b4327af43407e2c96d775b0b2b59
KL44S <agustinbinci@hotmail.com>
2015-05-22 14:45:24 -0300

    Primer commit

---
+++ RedDePalabras/src/RedDePalabras.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : RedDePalabras.cpp
+// Author      : KL44S
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
